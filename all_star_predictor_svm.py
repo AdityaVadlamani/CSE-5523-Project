29e7154f7e79144e17075d531d630ca2b7e6651a
surajsuresh1200 <31544664+surajsuresh1200@users.noreply.github.com>
2021-04-27 16:51:58 -0400

    Experimentation Method Changed

--- all_star_predictor_svm.py
+++ all_star_predictor_svm.py
@@ -3,86 +3,99 @@ import numpy
 import pandas
 from sklearn.model_selection import train_test_split
 from sklearn import svm
+from sklearn.utils import resample, shuffle
 import time
 
 era = int(sys.argv[1])
 
-YX = pandas.read_csv( "data/season_stats_AS.csv" )
+YX = pandas.read_csv( "data/season_stats_AS_records_norm_2.csv" )
 
 if era == 1:
-    feature_list = ['Age', 'G', 'TS%', 'FTr', 'WS', 'FGA', 'FG%', 'eFG%', 'FTA', 'FT%', 'TRB', 'AST', 'PTS']
+    feature_list = ['Age', 'G', 'TS%', 'FTr', 'WS', 'FG%', 'eFG%', 'FTA', 'FT%', 'TRB', 'AST', 'PTS']
 elif era == 2:
-    feature_list = ['Age', 'G', 'TS%', 'FTr', 'WS', 'FGA', 'FG%', 'eFG%', 'FTA', 'FT%', 'TRB', 'AST', 'PTS', 'MP', 'PER', 'STL', 'BLK']
+    feature_list = ['Age', 'G', 'TS%', 'FTr', 'WS', 'FG%', 'eFG%', 'FTA', 'FT%', 'TRB', 'AST', 'PTS', 'MP', 'PER', 'STL', 'BLK']
     YX = YX[YX['Year']>=1974]
 elif era == 3:
-    feature_list = ['Age', 'G', 'TS%', 'FTr', 'WS', 'FGA', 'FG%', 'eFG%', 'FTA', 'FT%', 'TRB', 'AST', 'PTS', 'MP', 'PER', 'STL', 'BLK', 'USG%', '3PA', '3P%']
+    feature_list = ['Age', 'G', 'TS%', 'FTr', 'WS', 'FG%', 'eFG%', 'FTA', 'FT%', 'TRB', 'AST', 'PTS', 'MP', 'PER', 'STL', 'BLK', 'USG%', '3PA', '3P%']
     YX = YX[YX['Year']>=1980]
     
 YX.dropna(subset = feature_list, inplace=True)
 YX.reset_index(drop=True, inplace=True)
 
-Y = YX['AS'].to_numpy().astype('int')
+YX_pos = YX[YX.AS == 1]
+YX_neg = YX[YX.AS == 0]
 
-X = YX[feature_list]
+YX_pos_upsampled = resample(YX_pos, replace=True, n_samples = len(YX_neg), random_state = 0)
 
-print(Y)
-print(X)
+YX_upsampled = shuffle(pandas.concat([YX_neg, YX_pos_upsampled], ignore_index=True), random_state = 42)
 
-Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size = 0.3, random_state = 42)
+Y = YX_upsampled['AS'].to_numpy().astype('int')
 
-model = svm.SVC()
+X = YX_upsampled[feature_list]
 
-start = time.time()
+precisions = []
+recalls = []
+f1s = []
 
-model.fit(Xtrain.values, Ytrain)
+for trial in range(10):
 
-end = time.time()
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size = 0.3, random_state = trial)
 
-correct = 0
-false_pos = 0
-false_neg = 0
-for i, x in enumerate(Xtest.values):
-    yhat = model.predict(x.reshape(1, -1))
-    if Ytest[i] == yhat:
-        correct += 1
-    else:
-        if yhat == 0:
-            false_neg+=1
+    model = svm.SVC(kernel = 'rbf')
+
+    start = time.time()
+
+    model.fit(Xtrain.values, Ytrain)
+
+    end = time.time()
+
+    correct = 0
+    false_pos = 0
+    false_neg = 0
+    true_pos = 0
+    true_neg = 0
+    
+    for i, x in enumerate(Xtest.values):
+        yhat = model.predict(x.reshape(1, -1))
+        if Ytest[i] == yhat:
+            correct += 1
+            if yhat == 0:
+                true_neg += 1
+            else:
+                true_pos += 1
         else:
-            false_pos+=1
+            if yhat == 0:
+                false_neg += 1
+            else:
+                false_pos += 1
+                
+    EPSILON = 1E-7
+    precision = (true_pos)/(true_pos + false_pos + EPSILON)
+    recall = (true_pos)/(true_pos + false_neg + EPSILON)
+    F1 = 2 * (precision * recall)/(precision + recall + EPSILON)
+    print(model.coef_)
+    
+    precisions.append(precision)
+    recalls.append(recall)
+    f1s.append(F1)
+    
         
-false_pos_tot = 0
-false_neg_tot = 0
-correct_tot = 0
-for i, player in YX.iterrows():
-    x = player[feature_list]
-    prediction = model.predict(x.to_numpy().reshape(1, -1))
-    if prediction == 1:
-        print(player['Player'], int(player['Year']))
-        print('True Label: {}'.format(player['AS']))
-        if int(player['AS']) == 0:
-            false_pos_tot += 1
-        else:
-            correct_tot += 1
-    else:
-        if int(player['AS']) == 1:
-            false_neg_tot += 1
-        else:
-            correct_tot += 1
-        
 
-print("\nTest Accuracy: {} out of {} ({})".format(correct, len(Xtest), correct/len(Xtest)))
-print("False Positives on Test Set: {}".format(false_pos))
-print("False Negatives on Test Set: {}".format(false_neg))
-print("\nComplete Accuracy: {} out of {} ({})".format(correct_tot, len(YX), correct_tot/len(YX)))
-print("False Positives on Complete Set: {}".format(false_pos_tot))
-print("False Negatives on Complete Set: {}".format(false_neg_tot))
-print("\nTime elapsed: {} seconds".format(end - start))
+    print("\nTrial: {}".format(trial + 1))
+    #print("Test Accuracy: {} out of {} ({})".format(correct, len(Xtest), correct/len(Xtest)))
+    #print("False Positive rate on Test Set: {}/{} (= {})".format(false_pos, total_neg, false_pos / total_neg))
+    #print("False Negative rate on Test Set: {}/{} (= {})".format(false_neg, total_pos, false_neg / total_pos))
 
-print("\nTest False Rates:")
-num_true = sum(Ytest)
-num_false = len(Ytest)-sum(Ytest)
-print("Total True: {}".format(num_true))
-print("Total False: {}".format(num_false))
-print("False Positive Rate: {}".format(false_pos/num_false))
-print("False Negative Rate: {}".format(false_neg/num_true))
+    print("Test Precision: {}/{} (= {})".format(true_pos, true_pos + false_pos, precision))
+    print("Test Recall: {}/{} (= {})".format(true_pos, true_pos + false_neg, recall))
+    print("Test F1 Score: {}".format(F1))
+
+    print("\nTime elapsed: {} seconds".format(end - start))
+    
+print("\nAverages:")
+
+print("Test Precision: {}".format(numpy.average(precisions)))
+print("Test Recall: {}".format(numpy.average(recalls)))
+print("Test F1 Score: {}".format(numpy.average(f1s)))
+
+
